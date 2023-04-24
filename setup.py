51aae28014886af9c92fb574f7298666397b594b
Nicolai Hartvig Sørensen <nicolaihs@gmail.com>
2023-04-24 11:42:29 +0200

    Fixed error in setup.py and added missing MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 """Setup.py for DSLLex."""
 from setuptools import setup, find_packages
+
 # List of requirements
 requirements = []  # This could be retrieved from requirements.txt
 # Package (minimal) configuration
 setup(
-    name="LexTools",
+    name="DSLSplit",
     version="0.1.0",
-    description="A FastAPI webservices for misc. nlp resources",
+    description="A FastAPI webservices for splitting Danish compound words",
     packages=find_packages(),  # __init__.py folders search
-    install_requires=requirements
+    install_requires=requirements,
+    include_package_data=True,
 )
